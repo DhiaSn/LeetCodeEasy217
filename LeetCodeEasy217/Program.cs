e5453ce6e01c02fc4e000f586ec6851091c5bb13
DhiaEddine.Snoussi <DhiaEddine.Snoussi@studentambassadors.com>
2024-08-02 13:14:00 +0100

    Add project files.

---
+++ LeetCodeEasy217/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace LeetCodeEasy217
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result1 = Solution.ContainsDuplicate([1, 2, 3, 1]);
+            if (!result1)
+            {
+                Console.WriteLine($"result1 is: {result1} instead of true");
+                return;
+            }
+            bool result2 = Solution.ContainsDuplicate([1, 2, 3, 4]);
+            if (result2)
+            {
+                Console.WriteLine($"result2 is: {result2} instead of false");
+                return;
+            }
+
+            bool result3 = Solution.ContainsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]);
+            if (!result3)
+            {
+                Console.WriteLine($"result3 is: {result3} instead of false");
+                return;
+            }
+
+            bool result4 = Solution.ContainsDuplicate([0]);
+            if (result4)
+            {
+                Console.WriteLine($"result4 is: {result4} instead of false");
+                return;
+            }
+
+            Console.WriteLine($"it works perfectly!");
+        }
+    }
+}
---
+++ LeetCodeEasy217/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeEasy217
+{
+    public static class Solution
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> seenNumbers = [];
+
+            foreach (int num in nums)
+                if (!seenNumbers.Add(num))
+                    return true;
+                
+            return false; 
+        }
+    }
+}
